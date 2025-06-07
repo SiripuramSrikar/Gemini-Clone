dead076d32288db5a63406ca738b09821f04edc0
SiripuramSrikar <siripuramsrikar100705@gmail.com>
2025-06-07 18:08:38 +0530

    sidebar completed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from './components/sidebar/sidebar'
+const App = () => {
+  return (
+    <>
+    <Sidebar></Sidebar>
+    </>
+  )
+}
+
+export default App
